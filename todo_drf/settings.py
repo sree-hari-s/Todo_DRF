8022ec5e2331b9e31d45abe5f9a8b245a1baa828
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-16 22:40:53 +0530

    Run both server and client together
    Build of react created

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'todo_drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 CORS_ALLOWED_ORIGINS = [
 
