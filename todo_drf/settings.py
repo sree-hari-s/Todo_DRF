c433382bb0dc45cb1fbaf41141d60a63ac059884
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-16 20:29:38 +0530

    setting.py updated cors-headers added

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     
     'rest_framework',
-
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'todo_drf.urls'
@@ -123,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+
+]
